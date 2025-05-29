22338a31710aefacf0aaf504f414492924667388
Brian Chen <brianchen@Brians-Macbook.local>
2025-05-28 23:05:18 -0700

    feat: add progress bar

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbxZDo6ten58nPhw2kFbtkItzDJaEDzlgSuVxilqxmrdiGKIQUAq3KNYJKqaJMAvy0RcDA/exec";
 let allContacts = [];
 
+const loadingElement = document.getElementById("loading");
+const listElement = document.getElementById("contact-list");
+
 async function fetchAllContacts() {
+  loadingElement.style.display = "block";   // 顯示 loading
+  listElement.style.display = "none";       // 暫時隱藏清單
+
   const res = await fetch(API_URL);
   allContacts = await res.json();
   renderContacts(allContacts);
+
+  loadingElement.style.display = "none";    // 隱藏 loading
+  listElement.style.display = "block";      // 顯示清單
 }
 
 function renderContacts(list) {
